f27f676214a6ecd6df4c1c1b4ee36bdddd569fc7
JavierGarciaEstrada <baevaj@naver.com>
2023-03-16 07:28:06 -0600

    Se añade la clase DatosDePaciente y TestPaciente

--- src/DatosDePaciente.java
+++ src/DatosDePaciente.java
@@ -1,2 +1,74 @@
-package PACKAGE_NAME;public class DatosDePaciente {
+public class DatosDePaciente{
+private String name;
+private int age;
+private double weight;
+private int height;
+private char sex;
+
+    public DatosDePaciente() {
+    }
+
+    public DatosDePaciente(String name, int age, double weight, int height, char sex){
+    this.name = name;
+    this.age = age;
+    this.weight = weight;
+    this.height = height;
+    this.sex = sex;
+}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {this.sex = sex;}
+
+    public double IMC(){
+        double imc=(weight/(height * height))*10000;
+
+        return imc;
+
+    }
+    public String compotition(double imc){
+        if (imc <= 18.5){
+            return "Underweight";
+        }else if (imc >18.5 && imc <=24.9){
+            return "Normal";
+        } else if (imc >=25 && imc <=29.9) {
+            return "Overweight";
+        } else {
+            return "Obesity";
+        }
+    }
 }
--- src/TestPaciente.java
+++ src/TestPaciente.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class TestPaciente {
+public class TestPaciente {
+    public static void main(String[] args) {
+
+        DatosDePaciente paciente1 = new DatosDePaciente("javier", 18, 50, 175, 'm');
+
+        System.out.println("El paciente: " + paciente1.getName());
+        System.out.println("Con edad de " + paciente1.getAge() + " años");
+        System.out.println("De sexo: " + paciente1.getSex());
+        System.out.println("Con un peso de: " + paciente1.getWeight());
+        System.out.println("Y midiendo: " + paciente1.getHeight() + " cm de altura");
+        System.out.println("Teniendo un IMC de: " + paciente1.IMC());
+        System.out.println("Entra en la categoria de: " + paciente1.compotition(paciente1.IMC()) + " Segun su  IMC");
+
+
+    }
 }
